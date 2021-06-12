4b52fa65b17ceedbb60852e39b3dd22f9ea7d4a7
ernifarinarakhmi <ernifarinarakhmi@gmail.com>
2021-06-12 14:26:08 +0800

    Operasi Tipe String

--- Modul01/src/main.java
+++ Modul01/src/main.java
@@ -10,5 +10,27 @@ public class Main {
         boolean cantik = true;
 
         System.out.println(namaDepan + namaTengah + namaBelakang);
+
+
+    char[] uniskaChar = {'U','N','I','S','K','A'};
+    String uniskaString = new String(uniskaChar);
+    System.out.println(uniskaString);
+
+    String namaLengkap = namaDepan + " " +
+            namaTengah + " " + namaBelakang;
+    System.out.println(namaLengkap.length());
+    System.out.println(namaLengkap.indexOf("NIS"));
+    System.out.println(namaLengkap.substring(5));
+    System.out.println(namaLengkap.substring(5,12));
+    System.out.println(namaLengkap.replace( "Farina", "Rakhmi"));
+    System.out.println(namaLengkap.toUpperCase());
+    System.out.println(namaLengkap.toLowerCase());
+    System.out.println(namaLengkap.charAt(6));
+
+    String[] namaArray = namaLengkap.split("");
+
+    for(String nama : namaArray){
+        System.out.print(nama);
+    }
     }
 }
